2d4f7cacffc808c678b40b6094c032612c1cd111
dinte.maria <dinte.maria@gmail.com>
2015-05-14 19:07:25 +0200

    project

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var tweets = [];
 
 
+
 router.get('/', function (req, res, next) {
     res.render('index', { tweets: tweets } );
 });
@@ -11,6 +12,24 @@ router.get('/', function (req, res, next) {
 router.post('/tweets', function (req, res, next) {
   tweets.unshift(req.body.tweetText);
   res.redirect('/');
+});
+
+router.get('/Welcome', function(req, res, next) {
+  res.render('Welcome', { tweets: tweets });
+});
+
+router.post('/Welcome', function(req, res, next) {
+  tweets.unshift(req.body.tweet);
+  res.redirect('Welcome')
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { tweets: tweets });
+});
+
+router.post('/index', function(req, res, next) {
+  tweets.unshift(req.body.tweet);
+  res.redirect('index')
 });
 
 module.exports = router;
